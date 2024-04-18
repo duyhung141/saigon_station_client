2204ab6db20e2111852e694df54eb19c743918ee
duyhung141 <leduyhung141@gmail.com>
2024-04-18 14:43:44 +0700

    update reponsive

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -3,20 +3,26 @@ import Header from "../../components/Header/Header";
 import homepage_introduce_1 from "../../assets/image/homepage_introduce_1.png";
 import homepage_introduce_2 from "../../assets/image/homepage_introduce_2.png";
 import homepage_introduce_3 from "../../assets/image/homepage_introduce_3.png";
+import shrimp from "../../assets/image/shrimp.jpg";
+import pho from "../../assets/image/pho.jpg";
+import table_food from "../../assets/image/table_food.jpg";
+import FoodBankIcon from '@mui/icons-material/FoodBank';
+import BrunchDiningIcon from '@mui/icons-material/BrunchDining';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
 import tree from "../../assets/image/tree.png";
 import lecture from "../../assets/image/lecture.png";
 import {Link} from "react-router-dom";
-
+import './homepage.css'
 function Homepage() {
     return (
         <>
             <div className="">
                 {/*<Header/>*/}
                 <div className="container mx-auto py-3">
-                    <div className="py-5 mb-5">
-                        <div className="grid grid-cols-12">
-                            <div className="col-span-4">
-                                <h1 className="font-bold uppercase text-[5rem] text-[#5E6600]">Introduce</h1>
+                    <div className="p-10 mb-5">
+                        <div className="xl:grid xl:grid-cols-12">
+                            <div className="xl:col-span-4">
+                                <h1 className="font-bold uppercase text-2xl text-[#5E6600]">Introduce</h1>
                                 <p className="text-justify">Emerging yourself in the journey of discovering Vietnamese
                                     Cuisines in Dubai either at
                                     the Bank of Business Bay Dubai Canal or at the Heart of Dubai Silicon Oasis.
@@ -33,13 +39,13 @@ function Homepage() {
                                 {/*</div>*/}
                             </div>
                             <div className="col-span-1"></div>
-                            <div className="col-span-7">
+                            <div className="xl:col-span-7 xl:block hidden">
                                 <div className="mt-3 grid grid-cols-3">
                                     <div className="">
                                         <img className="object-cover" src={homepage_introduce_2}
                                              alt="Saigon Station Food Introduce"/>
                                     </div>
-                                    <div className=" ">
+                                    <div className="col-span-1">
                                         <img className="object-cover" src={homepage_introduce_1}
                                              alt="Saigon Station Food Introduce"/>
                                     </div>
@@ -47,7 +53,22 @@ function Homepage() {
                                         <img className="absolute translate-x-1/2 -bottom-5 object-cover" src={homepage_introduce_3}
                                              alt="Saigon Station Food Introduce"/>
                                     </div>
-
+                                </div>
+                            </div>
+                            <div className="xl:hidden">
+                                <div className="mt-5 flex gap-2 ">
+                                    <div className="">
+                                        <img className="w-24 h-24 object-cover rounded-full" src={homepage_introduce_2}
+                                             alt="Saigon Station Food Introduce"/>
+                                    </div>
+                                    <div className="">
+                                        <img className="w-24 h-24 object-cover rounded-full" src={homepage_introduce_1}
+                                             alt="Saigon Station Food Introduce"/>
+                                    </div>
+                                    <div className="">
+                                        <img className="w-24 h-24 object-cover rounded-full" src={homepage_introduce_3}
+                                             alt="Saigon Station Food Introduce"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -55,94 +76,114 @@ function Homepage() {
                     </div>
                 </div>
 
-                <div className="mb-5 sm:grid sm:grid-cols-3">
-                    <div className="sm:col-span-1 w-full">
-                        <Link>
-                            <div className="relative group w-full h-[20rem]">
-                                <img className="w-full h-full object-cover" src={homepage_introduce_2}
-                                     alt="Saigon Station Food Introduce"/>
-                                <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
-                                <p className="absolute rounded top-[45%] left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
-                                    details </p>
-                            </div>
-                        </Link>
-                    </div>
+                {false && <>
+                    <div className="mb-5 sm:grid sm:grid-cols-3">
+                        <div className="sm:col-span-1 w-full">
+                            <Link>
+                                <div className="relative group w-full h-[20rem]">
+                                    <img className="w-full h-full object-cover" src={homepage_introduce_2}
+                                         alt="Saigon Station Food Introduce"/>
+                                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
+                                    <p className="absolute rounded top-[45%] left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
+                                        details </p>
+                                </div>
+                            </Link>
+                        </div>
 
-                    <div className="sm:col-span-1 w-full sm:grid sm:grid-cols-2">
-                        <div className="">
-                            <Link>
-                                <div className="relative group w-full h-[10rem] ">
-                                    <img className="w-full h-full object-cover" src={homepage_introduce_2}
-                                         alt="Saigon Station Food Introduce"/>
-                                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
-                                    <p className="absolute rounded top-1/3 left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
-                                        Details</p>
-                                </div>
-                            </Link>
-                        </div>
-                        <div className="">
-                            <Link>
-                                <div className="relative group w-full h-[10rem] ">
-                                    <img className="w-full h-full object-cover" src={homepage_introduce_2}
-                                         alt="Saigon Station Food Introduce"/>
-                                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
-                                    <p className="absolute rounded top-1/3 left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
-                                        Details</p>
-                                </div>
-                            </Link>
-                        </div>
-                        <div className="">
-                            <Link>
-                                <div className="relative group w-full h-[10rem] ">
-                                    <img className="w-full h-full object-cover" src={homepage_introduce_2}
-                                         alt="Saigon Station Food Introduce"/>
-                                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
-                                    <p className="absolute rounded top-1/3 left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
-                                        Details</p>
-                                </div>
-                            </Link>
-                        </div>
-                        <div className="">
-                            <Link>
-                                <div className="relative group w-full h-[10rem] ">
-                                    <img className="w-full h-full object-cover" src={homepage_introduce_2}
-                                         alt="Saigon Station Food Introduce"/>
-                                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
-                                    <p className="absolute rounded top-1/3 left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
-                                        Details</p>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="sm:col-span-1 w-full">
-                        <Link>
-                            <div className="relative group w-full h-[20rem] ">
-                                <img className="w-full h-full object-cover" src={homepage_introduce_2}
-                                     alt="Saigon Station Food Introduce"/>
-                                <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
-                                <p className="absolute rounded top-[45%] left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
-                                    Details</p>
+                        <div className="sm:col-span-1 w-full sm:grid sm:grid-cols-2">
+                            <div className="">
+                                <Link>
+                                    <div className="relative group w-full h-[10rem] ">
+                                        <img className="w-full h-full object-cover" src={homepage_introduce_2}
+                                             alt="Saigon Station Food Introduce"/>
+                                        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
+                                        <p className="absolute rounded top-1/3 left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
+                                            Details</p>
+                                    </div>
+                                </Link>
                             </div>
-                        </Link>
+                            <div className="">
+                                <Link>
+                                    <div className="relative group w-full h-[10rem] ">
+                                        <img className="w-full h-full object-cover" src={homepage_introduce_2}
+                                             alt="Saigon Station Food Introduce"/>
+                                        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
+                                        <p className="absolute rounded top-1/3 left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
+                                            Details</p>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link>
+                                    <div className="relative group w-full h-[10rem] ">
+                                        <img className="w-full h-full object-cover" src={homepage_introduce_2}
+                                             alt="Saigon Station Food Introduce"/>
+                                        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
+                                        <p className="absolute rounded top-1/3 left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
+                                            Details</p>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link>
+                                    <div className="relative group w-full h-[10rem] ">
+                                        <img className="w-full h-full object-cover" src={homepage_introduce_2}
+                                             alt="Saigon Station Food Introduce"/>
+                                        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
+                                        <p className="absolute rounded top-1/3 left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
+                                            Details</p>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-1 w-full">
+                            <Link>
+                                <div className="relative group w-full h-[20rem] ">
+                                    <img className="w-full h-full object-cover" src={homepage_introduce_2}
+                                         alt="Saigon Station Food Introduce"/>
+                                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
+                                    <p className="absolute rounded top-[45%] left-1/2 -translate-x-1/2 bg-[#6B4E4E] text-white p-3 transform duration-300 group-hover:underline">View
+                                        Details</p>
+                                </div>
+                            </Link>
+                        </div>
                     </div>
+                </>}
+                <div className="w-screen px-10 py-16 mb-5 intro relative">
+                    <div className="grid md:grid-cols-3 md:space-y-0 space-y-10 relative z-10">
+                        <div className="flex-col items-center justify-center text-center">
+                            <FoodBankIcon sx={{ fontSize: 100 }} color={'warning'}/>
+                            <p style={{fontFamily:"Roboto"}} className='text-white text-lg p-1'>2 Branchs</p>
+                        </div>
+                        <div className="flex-col items-center justify-center text-center">
+                            <BrunchDiningIcon sx={{ fontSize: 100 }} color={'warning'}/>
+                            <p style={{fontFamily:"Roboto"}} className='text-white text-lg p-1'>90+ Items of Food</p>
+                        </div>
+                        <div className="flex-col items-center justify-center text-center">
+                            <LocalDiningIcon sx={{ fontSize: 100 }} color={'warning'}/>
+                            <p style={{fontFamily:"Roboto"}} className='text-white text-lg p-1'>Professional Chefs</p>
+                        </div>
+                    </div>
+                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-80"></div>
+
                 </div>
 
-                <div className="container mx-auto py-3">
-                    <div className="grid md:grid-cols-2">
-                        <div className="flex gap-3 justify-center relative">
-                            <div className="absolute -left-10 -translate-x-1/2">
-                                <img className="h-[20rem] object-cover" src={tree} alt=""/>
+                <div className="container mx-auto my-10 p-10">
+                    <div className="lg:flex justify-center items-center gap-3 relative">
+                        <div className="absolute top-0 -left-[15vh]">
+                            <img className="h-[20rem] object-cover" src={tree} alt=""/>
+                        </div>
+                        <div className="flex order-2 gap-3 justify-center ">
+                            <div className="md:w-[15rem] w-[9rem] mx-auto">
+                                <img className="w-full object-cover rounded" src={pho} alt=""/>
                             </div>
-                            <div className="w-[15rem]">
-                                <img className="w-full object-cover" src={homepage_introduce_2} alt=""/>
-                            </div>
-                            <div className="w-[15rem]">
-                                <img className="w-full object-cover" src={homepage_introduce_2} alt=""/>
+                            <div className="md:w-[15rem] w-[9rem] mx-auto">
+                                <img className="w-full object-cover rounded" src={homepage_introduce_2} alt=""/>
                             </div>
                         </div>
 
-                        <div className=" my-auto">
+                        <div className="lg:mt-0 mt-5 order-1">
                             <p className="text-[#A1A1A1]">FOR YOU <span
                                 className="block h-1 bg-gradient-to-r from-gray-400 to-transparent w-32"></span>
                             </p>
--- src/pages/Menupage/Menupage.jsx
+++ src/pages/Menupage/Menupage.jsx
@@ -1,18 +1,63 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import Slider from 'react-slick';
 import "./carousel.css"
-import menu1 from "../../assets/image/menu/menu-update_page-0001.jpg";
-import menu2 from "../../assets/image/menu/menu-update_page-0002.jpg";
-import menu3 from "../../assets/image/menu/menu-update_page-0003.jpg";
-import menu4 from "../../assets/image/menu/menu-update_page-0004.jpg";
-import menu5 from "../../assets/image/menu/menu-update_page-0005.jpg";
-import menu6 from "../../assets/image/menu/menu-update_page-0006.jpg";
-import menu7 from "../../assets/image/menu/menu-update_page-0007.jpg";
-import menu8 from "../../assets/image/menu/menu-update_page-0008.jpg";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import menu1 from '../../assets/image/menu/menu-update_page-0001.jpg';
+import menu2 from '../../assets/image/menu/menu-update_page-0002.jpg';
+import menu3 from '../../assets/image/menu/menu-update_page-0003.jpg';
+import menu4 from '../../assets/image/menu/menu-update_page-0004.jpg';
+import menu5 from '../../assets/image/menu/menu-update_page-0005.jpg';
+import menu6 from '../../assets/image/menu/menu-update_page-0006.jpg';
+import menu7 from '../../assets/image/menu/menu-update_page-0007.jpg';
+import menu8 from '../../assets/image/menu/menu-update_page-0008.jpg';
+import menu9 from '../../assets/image/menu/menu-update_page-0009.jpg';
+import menu10 from '../../assets/image/menu/menu-update_page-0010.jpg';
+import menu11 from '../../assets/image/menu/menu-update_page-0011.jpg';
+import menu12 from '../../assets/image/menu/menu-update_page-0012.jpg';
+import menu13 from '../../assets/image/menu/menu-update_page-0013.jpg';
+import menu14 from '../../assets/image/menu/menu-update_page-0014.jpg';
+import menu15 from '../../assets/image/menu/menu-update_page-0015.jpg';
+import menu16 from '../../assets/image/menu/menu-update_page-0016.jpg';
+import menu17 from '../../assets/image/menu/menu-update_page-0017.jpg';
+import menu18 from '../../assets/image/menu/menu-update_page-0018.jpg';
+import menu19 from '../../assets/image/menu/menu-update_page-0019.jpg';
+import menu20 from '../../assets/image/menu/menu-update_page-0020.jpg';
+import menu21 from '../../assets/image/menu/menu-update_page-0021.jpg';
+import menu22 from '../../assets/image/menu/menu-update_page-0022.jpg';
+import menu23 from '../../assets/image/menu/menu-update_page-0023.jpg';
+import menu24 from '../../assets/image/menu/menu-update_page-0024.jpg';
+import menu25 from '../../assets/image/menu/menu-update_page-0025.jpg';
+import menu26 from '../../assets/image/menu/menu-update_page-0026.jpg';
+import menu27 from '../../assets/image/menu/menu-update_page-0027.jpg';
+import menu28 from '../../assets/image/menu/menu-update_page-0028.jpg';
+import menu29 from '../../assets/image/menu/menu-update_page-0029.jpg';
+import menu30 from '../../assets/image/menu/menu-update_page-0030.jpg';
+import menu31 from '../../assets/image/menu/menu-update_page-0031.jpg';
+import menu32 from '../../assets/image/menu/menu-update_page-0032.jpg';
+import menu33 from '../../assets/image/menu/menu-update_page-0033.jpg';
+import menu34 from '../../assets/image/menu/menu-update_page-0034.jpg';
+import menu35 from '../../assets/image/menu/menu-update_page-0035.jpg';
+import menu36 from '../../assets/image/menu/menu-update_page-0036.jpg';
+import menu37 from '../../assets/image/menu/menu-update_page-0037.jpg';
+import menu38 from '../../assets/image/menu/menu-update_page-0038.jpg';
+import menu39 from '../../assets/image/menu/menu-update_page-0039.jpg';
+import menu40 from '../../assets/image/menu/menu-update_page-0040.jpg';
+import menu41 from '../../assets/image/menu/menu-update_page-0041.jpg';
+import menu42 from '../../assets/image/menu/menu-update_page-0042.jpg';
+import menu43 from '../../assets/image/menu/menu-update_page-0043.jpg';
+import menu44 from '../../assets/image/menu/menu-update_page-0044.jpg';
+import menu45 from '../../assets/image/menu/menu-update_page-0045.jpg';
+import menu46 from '../../assets/image/menu/menu-update_page-0046.jpg';
+import menu47 from '../../assets/image/menu/menu-update_page-0047.jpg';
+import menu48 from '../../assets/image/menu/menu-update_page-0048.jpg';
+import menu49 from '../../assets/image/menu/menu-update_page-0049.jpg';
+import menu50 from '../../assets/image/menu/menu-update_page-0050.jpg';
+
 
 function Menupage() {
     const settings = {
-        dots: true,
+        dots: false,
         infinite: false,
         speed: 500,
         slidesToShow: 2,
@@ -24,14 +69,41 @@ function Menupage() {
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                }
+                },
+
             },
         ]
     };
+    const menuImages = [menu1, menu2, menu3, menu4, menu5, menu6, menu7, menu8, menu9, menu10,
+        menu11, menu12, menu13, menu14, menu15, menu16, menu17, menu18, menu19, menu20, menu21,
+        menu22, menu23, menu24, menu25, menu26, menu27, menu28, menu29, menu30, menu31, menu32,
+        menu33, menu34, menu35, menu36, menu37, menu38, menu39, menu40, menu41, menu42, menu43,
+        menu44, menu45, menu46, menu47, menu48, menu49, menu50];
+
+
+    let sliderRef = useRef(null);
+    const next = () => {
+        sliderRef.slickNext();
+    };
+    const previous = () => {
+        sliderRef.slickPrev();
+    };
+
     return (
-        <div className="bg-[#7D9980] mx-auto">
-            <div className="slide-container xl:max-w-[70rem] lg:max-w-[55rem] md:max-w-[40rem] max-w-[15rem] mx-auto">
-                <Slider {...settings}>
+        <div className="bg-[#7D9980] mx-auto p-10">
+            <div
+                className="slide-container relative xl:max-w-[70rem] lg:max-w-[55rem] md:max-w-[40rem] max-w-[15rem] mx-auto">
+                <Slider
+                    ref={slider => {
+                        sliderRef = slider;
+                    }}
+                    {...settings}
+                >
+                    {
+                        menuImages.map((menuImage, index) => (
+                            <img key={index} className="sm:h-screen" src={menuImage} alt={"menu" + index}/>
+                        ))
+                    }
                     <img className="sm:h-screen" src={menu1} alt=""/>
                     <img className="sm:h-screen" src={menu2} alt=""/>
                     <img className="sm:h-screen" src={menu3} alt=""/>
@@ -41,6 +113,18 @@ function Menupage() {
                     <img className="sm:h-screen" src={menu7} alt=""/>
                     <img className="sm:h-screen" src={menu8} alt=""/>
                 </Slider>
+                <button className="absolute top-1/2 -translate-y-1/2 right-0" onClick={next}>
+                    <span
+                        className="material-icons material-icons-outlined text-[#2e7d32] lg:text-4xl inline-flex justify-center items-center ml-4"
+                    >arrow_forward_ios</span>
+                    {/*<ArrowForwardIosIcon fontSize={'large'} color={'success'}/>*/}
+                </button>
+
+                <button className="absolute top-1/2 -translate-y-1/2 left-0" onClick={previous}>
+                <span
+                    className="material-icons material-icons-outlined text-[#2e7d32] lg:text-4xl inline-flex justify-center items-center ml-2"
+                >arrow_back_ios</span>
+                </button>
             </div>
         </div>
     )
