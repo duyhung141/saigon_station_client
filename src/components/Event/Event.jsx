efbef74ad912dd5b80cb14f97c10273cd4a5cb22
duyhung141 <leduyhung141@gmail.com>
2024-04-25 00:20:50 +0700

    update

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -14,7 +14,7 @@ function Event(props) {
                     <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-2">{data?.tittle}</h5>
                 </Link>
                 <p className="mb-3 font-normal text-sm text-gray-700 dark:text-gray-400 line-clamp-3">{data?.description}</p>
-                <Link to={`/event/${data?._id}`}>
+                <Link to={`/event/${data?._id}`}
                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
